7ff68f7858bbb7283be0a98ce894c28bb9d8ea92
Sasuke Uchiha <97214534+Sasuke031026@users.noreply.github.com>
2022-05-30 15:22:46 +1000

    update

--- server.js
+++ server.js
@@ -39,5 +39,5 @@ app.get("/*", function (req, res) {
     });
 });
 
-const port = process.env.SERVER_PORT || 3306;
+const port = process.env.SERVER_PORT || 5009;
 app.listen(port, () => console.log(`Running on port ${port}`));
